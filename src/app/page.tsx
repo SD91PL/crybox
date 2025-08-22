18727798601370145453028911cd27bc65dbc796
SD91PL <sd91pl@gmail.com>
2025-08-22 15:15:29 +0200

    style: update global font to Bahnschrift

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Counter from '@/components/Counter'
 export default function Home() {
 	return (
 		<div className='container bg-gray-700'>
-			<div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
+			<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
 				<main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
 					<div className='flex gap-4 items-center flex-col sm:flex-row'>
 						<div>
