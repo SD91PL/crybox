43403cc05e0b542e746e69404144f2bc20acd3ac
SD91PL <sd91pl@gmail.com>
2025-09-05 11:38:45 +0200

    style: enhance header images with animations and priority loading
    - added animations to header images
    
    - set 'priority' attribute for faster Next.js image loading
    
    - updated global filter transitions for smoother UX

--- src/UI/Path.tsx
+++ src/UI/Path.tsx
@@ -14,7 +14,7 @@ export default function Path({ children }: PathProps) {
 			)
 
 	return (
-		<PathBg className='inline-flex items-center justify-between pl-5 pr-3 py-2.5 bg-[#6a6a6a] rounded-xl break-words'>
+		<PathBg className='inline-flex items-center justify-between pl-5 pr-3 py-2.5 bg-[#6a6a6a] rounded-xl break-words transition-filter'>
 			<code className='font-sans font-normal text-white text-sm'>
 				{formatTextWithBreaks(children)}
 			</code>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,19 @@ export default function Header() {
 				src='/img/header/crybox_logo.png'
 				alt='Crybox - Maximum Control'
 				fill
-				className='object-contain'
+				priority
+				className='object-contain fade-in-top'
 			/>
-			<div className='absolute bottom-2 xl:bottom-10 h-1/3 w-full flex flex-col justify-center items-center'>
-				<a href='#mapping' className='min-w-[max(160px,14%)] min-h-[68px]'>
+			<div className='absolute bottom-2 xl:bottom-10 h-1/3 w-full flex flex-col justify-center items-center fade-in-bottom'>
+				<a
+					href='#mapping'
+					className='min-w-[max(160px,14%)] min-h-[68px]'>
 					<Image
 						src='/img/header/engage.png'
 						alt='Engage'
 						width={160}
 						height={68}
+						priority
 						className='w-full h-full'
 					/>
 				</a>
